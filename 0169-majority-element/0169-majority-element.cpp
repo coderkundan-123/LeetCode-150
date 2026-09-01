e9fa26a0d2ce7efc8f33cbd510df0b42418e3b74
Kundan Kumar <kundan027427@gmail.com>
2026-09-02 02:37:06 +0530

    Time: 0 ms (100%), Space: 28.3 MB (44.63%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
-// T.C = O(n log n)
-// S.C = O(n)
+// T.C = O(n)
+// S.C = O(1)
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        map<int,int>mp;
+        int cnt = 0;
+        int ele = 0;
         for(int i=0; i<n; i++){
-            mp[nums[i]]++;
-        }
-
-        for(auto it : mp){
-            if(it.second > n/2){
-                return it.first;
+            if(cnt == 0){
+                cnt = 1;
+                ele = nums[i];
+            }else if(nums[i] == ele){
+                cnt++;
+            }else {
+                cnt--;
             }
+        }
+        int cnt1 = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] == ele)cnt1++;
+        }
+        if(cnt1 > n/2){
+            return ele;
         }
         return -1;
     }
