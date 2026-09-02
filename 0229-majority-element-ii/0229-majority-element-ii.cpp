4badd0ce4468892aa9c0dea4612ae7bd96811f1d
Kundan Kumar <kundan027427@gmail.com>
2026-09-02 21:22:01 +0530

    Implement optimal majority element solution in C++
    Added an optimal solution for finding majority elements in an array.

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -27,3 +27,60 @@ public:
         return list;
     }
 };
+
+/*********************** most Optimal Solution in cpp **************************/
+class Solution {
+public:
+        /*  ⏱️ Time Complexity
+
+        There are 3 main operations:
+
+        First for loop → O(n)
+        Second for loop → O(n)
+        sort(li.begin(), li.end())
+
+        Important point: li can contain at most 2 elements, because elements occurring more than n/3 can be at most 2.
+
+        So sorting li is:
+
+        O(2 log 2) = O(1)
+
+        Therefore:
+
+        Total T.C = O(n) + O(n) + O(1) = O(n)
+            S.C = O(1) 
+
+        */
+    vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        int cnt1 = 0, cnt2 = 0;
+        int ele1 = 0, ele2 = 0;
+
+        for(int i=0; i<n; i++){ // O(n)
+            if(cnt1 == 0 && nums[i] != ele2){
+                cnt1++, ele1 = nums[i];
+            }else if(cnt2 == 0 && nums[i] != ele1){
+                cnt2++, ele2 = nums[i];
+            }else if(ele1 == nums[i]){
+                cnt1++;
+            }else if(ele2 == nums[i]){
+                cnt2++;
+            }else{
+                cnt1--,cnt2--;
+            }
+        }
+
+        vector<int> li;
+        cnt1 = 0, cnt2=0;
+        for(int i=0; i<n; i++){ //O(n)
+            if(ele1 == nums[i])cnt1++;
+            if(ele2 == nums[i])cnt2++;
+        }
+        int mini = (int)(n/3) + 1;
+        if(cnt1 >= mini)li.push_back(ele1);
+        if(cnt2 >= mini)li.push_back(ele2);
+        sort(li.begin(), li.end());
+
+        return li;
+    }
+};
